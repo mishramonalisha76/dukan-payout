88ca6860faf2fd5c35e904f615d9c684e489f2a5
Monalisha Mishra <mishramonalisha76@gmail.com>
2024-01-09 21:32:15 +0530

    added finishing touches

--- src/components/Content/Transactions.tsx
+++ src/components/Content/Transactions.tsx
@@ -3,6 +3,8 @@ import { BiSortAlt2 } from "react-icons/bi";
 import { CiCircleInfo } from "react-icons/ci";
 import { FiDownload } from "react-icons/fi";
 import { FiSearch } from "react-icons/fi";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 export const Transactions = () => {
   const orders = [
@@ -74,12 +76,12 @@ export const Transactions = () => {
     },
   ];
   return (
-    <div className="w-full h-full flex  items-center flex-col gap-5 px-[25px] pt-4">
+    <div className="w-full  h-full flex   flex-col gap-5 px-[25px] pt-4">
       <div className="flex justify-between w-full">
         <p className=" font-medium text-lg">Transactions | This Month</p>
       </div>
 
-      <div className=" bg-white flex flex-col gap-[6px] p-3 w-full rounded-[8px]  shadow-sm">
+      <div className=" bg-white items-center flex flex-col gap-[6px] p-3 w-full h-[76%] rounded-[8px]  shadow-sm">
         <div
           className="flex 
             justify-between  w-full pb-2
@@ -115,13 +117,13 @@ export const Transactions = () => {
           </div>
         </div>
         <div
-          className="flex 
+          className="flex  w-full
             justify-between  rounded-[4px] bg-[#f2f2f2] 
             px-2 py-2 "
         >
           <span className="text-sm">Order ID</span>
-          <span className="text-sm">Order Date</span>
-          <span className="text-sm">Order Amount</span>
+          <span className="text-sm ">Order Date</span>
+          <span className="text-sm ">Order Amount</span>
           <span className="text-sm flex gap-[1px] items-center">
             Transaction Fees{" "}
             <span>
@@ -143,6 +145,22 @@ export const Transactions = () => {
               <span className="text-sm">{order.fee}</span>
             </div>
           ))}
+        </div>
+        <div className="flex w-[50%] justify-between mt-4">
+            <div className="flex gap-1   bg-white text-[#4d4d4d] items-center justify-between border-[1px] border-[#d9d9d9]
+          px-2 py-1 text-sm rounded-[4px]">
+              <IoIosArrowBack/>
+                Previous
+            </div>
+            <div>
+
+            </div>
+            <div className=" bg-white text-[#4d4d4d] flex gap-1 items-center justify-between border-[1px] border-[#d9d9d9]
+          px-2 py-1 text-sm rounded-[4px]">
+                Next
+                <IoIosArrowForward/>
+            </div>
+
         </div>
       </div>
     </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { GoHome } from "react-icons/go";
+import { CiWallet } from "react-icons/ci";
 
 export const Sidebar = () => {
   const lists = [
@@ -54,7 +55,8 @@ export const Sidebar = () => {
     },
   ];
   return (
-    <div className="flex h-full bg-[#1E2640] w-[224px] justify-start px-[16px] py-[10px] gap-6 flex-col">
+    <div className="flex h-full bg-[#27304c] w-[224px] justify-between px-[16px] py-[10px] gap-6 flex-col">
+      <div className="flex gap-6 flex-col">
       <div className="flex gap-3 w-full items-start">
         <div className="w-[39px] h-[39px] bg-white rounded-md"></div>
         <div className="flex justify-between w-full items-center">
@@ -70,14 +72,22 @@ export const Sidebar = () => {
         </div>
       </div>
       <div className="flex flex-col gap-3 w-full px-1">
-        {lists.map((list,i) => (
+        {lists.map((list, i) => (
           <div key={`title${i}`} className="flex gap-2 items-center">
-            <span>
-            {list.icon}
-            </span>
+            <span>{list.icon}</span>
             <span className="text-white text-sm font-normal">{list.title}</span>
           </div>
         ))}
+      </div>
+      </div>
+      <div className="flex w-full gap-2 items-center  bg-[#313a58] rounded-[4px]  px-3 py-2">
+        <div className="bg-[#4b5880] rounded-[4px] flex items-center w-fit h-fit p-1 ">
+            <CiWallet color='#fff' size={30}/>
+        </div>
+        <div className="flex flex-col text-white  ">
+          <span className=" text-xs ">Available credits</span>
+          <span className=" text-md font-medium">222.10</span>
+        </div>
       </div>
     </div>
   );
